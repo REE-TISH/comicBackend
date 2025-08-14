f73da591b3a604ad8c349225e53a24396fc4a11c
REE-TISH <reetishk798@gmail.com>
2025-08-14 10:30:03 +0530

    Update views.py

--- Messages/views.py
+++ Messages/views.py
@@ -3,11 +3,18 @@ from .models import ComicGroupMessage
 from ComicData.models import ComicGroup
 from .serializers import ComicGroupMessageSerializer
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 class ComicGroupMessageListView(generics.ListAPIView):
     queryset = ComicGroupMessage.objects.all()
     serializer_class = ComicGroupMessageSerializer
+    pagination_class = CustomPageNumberPagination
 
 
     def get_queryset(self):
