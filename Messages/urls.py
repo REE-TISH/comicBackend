d7b06cf46b2d1dc0375256c834c45940a03e3c26
REE-TISH <reetishk798@gmail.com>
2025-08-18 20:22:25 +0530

    Update urls.py

--- Messages/urls.py
+++ Messages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('group/<str:group_id>', views.ComicGroupMessageListView.as_view(), name='comic-group-message-list'),
-    path('commentList/',views.CommentListView.as_view(),name='comment-list')
+    path('commentList/',views.CommentListView.as_view(),name='comment-list'),
+    path('addComments/',views.CommentAddView.as_view(),name='comment-add'),
 ]
