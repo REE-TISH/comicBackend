75594108a87de366ba9c779382331647cd3e1b7e
REE-TISH <reetishk798@gmail.com>
2025-08-21 16:40:31 +0530

    Update urls.py

--- Messages/urls.py
+++ Messages/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('group/<str:group_id>', views.ComicGroupMessageListView.as_view(), name='comic-group-message-list'),
     path('commentList/<str:comic_id>/<str:chapter_no>/',views.CommentListView.as_view(),name='comment-list'),
-    path('addComments/<str:chapter_id>/',views.CommentAddView.as_view(),name='comment-add'),
+    path('addComments/<str:comic_id>/<str:chapter_no>/',views.CommentAddView.as_view(),name='comment-add'),
     path('addInBoxComment/<str:msg_id>/',views.InBoxCommentAddView.as_view(),name='Inbox-comment'),
 ]
